0c0d8616ba4278f69da143174f5035512cc3877b
zephyrkul <zephyrkul@users.noreply.github.com>
2025-03-05 15:35:54 -0500

    URL helper method fixes

--- sans/_url.py
+++ sans/_url.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
 from datetime import date as _date
-from typing import TYPE_CHECKING, Dict, Mapping, NewType
+from itertools import starmap
+from typing import TYPE_CHECKING, Callable, Dict, Iterable, Mapping, NewType
 
 import httpx
 
 if TYPE_CHECKING:
-    from typing_extensions import Literal
+    from typing_extensions import Literal, TypeAlias, TypeVar
+
+    _T = TypeVar("_T")
 
     _Shard = NewType("_Shard", Dict[str, str])
+    _QueryValueTypes: TypeAlias = "str | int | float | bool"
 
 API_URL = httpx.URL("https://www.nationstates.net/cgi-bin/api.cgi")
 
@@ -20,6 +24,8 @@ __all__ = [
     "WA",
     "Command",
     "Shard",
+    "View",
+    "Range",
     "NationsDump",
     "RegionsDump",
     "CardsDump",
@@ -27,25 +33,53 @@ __all__ = [
 ]
 
 
-def Nation(nation: str, *shards: str | _Shard, **parameters: str) -> httpx.URL:
+class _encoder:
+    def nation(self, nation: _QueryValueTypes) -> str:
+        return str(nation).replace(" ", "_")
+
+    region = nation
+
+    def to(self, to: _T) -> _T | str:
+        if isinstance(to, str):
+            return self.nation(to)
+        return to
+
+    def q(self, q: _QueryValueTypes) -> str:
+        return str(q)
+
+    def __getattr__(self, _) -> Callable[[_T], _T]:
+        return lambda x: x
+
+    def __call__(
+        self, key: str, value: _QueryValueTypes
+    ) -> tuple[str, _QueryValueTypes]:
+        return key, getattr(self, key)(value)
+
+
+def Nation(
+    nation: str, *shards: str | _Shard, **parameters: _QueryValueTypes
+) -> httpx.URL:
     return World(*shards, nation=nation, **parameters)
 
 
-def Region(region: str, *shards: str, **parameters: str) -> httpx.URL:
+def Region(
+    region: str, *shards: str | _Shard, **parameters: _QueryValueTypes
+) -> httpx.URL:
     return World(*shards, region=region, **parameters)
 
 
-def World(*shards: str | _Shard, **parameters: str) -> httpx.URL:
-    q: list[str | None] = [parameters.pop("q", None)]
-    query: dict[str, str] = {}
+def World(*shards: str | _Shard, **parameters: _QueryValueTypes) -> httpx.URL:
+    encoder = _encoder()
+    q: list[_QueryValueTypes | None] = [parameters.pop("q", None)]
+    query: dict[str, _QueryValueTypes] = {}
     for shard in shards:
         if isinstance(shard, Mapping):
             shard = dict(shard)
             q.append(shard.pop("q", None))
-            query.update(shard)
+            query.update(starmap(encoder, shard.items()))
         else:
             q.append(str(shard))
-    q_str = " ".join(map(str, filter(None, q)))
+    q_str = " ".join(map(encoder.q, filter(None, q)))
     if q_str:
         query["q"] = q_str
     query.update(parameters)
@@ -53,12 +87,12 @@ def World(*shards: str | _Shard, **parameters: str) -> httpx.URL:
 
 
 def WA(
-    wa: Literal[1, "1", 2, "2"], *shards: str | _Shard, **parameters: str
+    wa: Literal[1, "1", 2, "2"], *shards: str | _Shard, **parameters: _QueryValueTypes
 ) -> httpx.URL:
     return World(*shards, wa=str(wa), **parameters)
 
 
-def Command(nation: str, c: str, **parameters: str) -> httpx.URL:
+def Command(nation: str, c: str, **parameters: _QueryValueTypes) -> httpx.URL:
     return World(nation=nation, c=c, **parameters)
 
 
@@ -66,9 +100,27 @@ def Telegram(client: str, tgid: str, key: str, to: str) -> httpx.URL:
     return World(a="sendtg", client=client, tgid=tgid, key=key, to=to)
 
 
-def Shard(q: str, **parameters: str) -> _Shard:
+def Shard(q: str, **parameters: _QueryValueTypes) -> _Shard:
     parameters["q"] = q
     return parameters  # type: ignore
+
+
+def View(*, nations: Iterable[str] = (), regions: Iterable[str] = ()) -> _Shard:
+    encoder = _encoder()
+    nations = ",".join(
+        map(encoder.nation, (nations,) if isinstance(nations, str) else nations)
+    )
+    regions = ",".join(
+        map(encoder.region, (regions,) if isinstance(regions, str) else regions)
+    )
+    view = " ".join(filter(len, (nations, regions)))
+    if view:
+        return {"view": view}  # type: ignore
+    return {}  # type: ignore
+
+
+def Range(__from: _QueryValueTypes, __to: _QueryValueTypes) -> _Shard:
+    return {"from": __from, "to": __to}  # type: ignore
 
 
 # https://www.nationstates.net/archive/nations/2018-09-30-nations-xml.gz
